937236dc544d8904a6ec3f375fabd95b13ba7b0b
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-11-02 12:54:54 -0500

    working on selector

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     function renderMovies(movieArray) {
-        finalHTML = ""
         moviesHTML = movieArray.map(function (currentMovie) {
             return `<div class="card" style="width: 18rem;">
             <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
             <div class="card-body">
-            <p class="card-text">${currentMovie.Title}</p>
+            <p class="card-text">${currentMovie.Title} ${currentMovie.Year}</p>
             </div>
             </div>`
         })
-        finalHTML += moviesHTML.join()
-        return finalHTML
+        return moviesHTML.join()
     }
-    // let content = document.getElementById("movie");
-    // content.innerHTML = (renderMovies(movieData))
-    
+      
+    let content = document.getElementsByClassName("movie")[0];
+    // document.getElementById("searchButton").addEventListener("submit", renderMovies(movieData))
+    // content.innerHTML = (renderMovies(movieData))   
+    $('#searchButton').click(function() {
+        content.innerHTML = renderMovies(movieData)
+    })
 })
 
