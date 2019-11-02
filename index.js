43fcca0c8d7ff839691a159c26e90faad8ecb1b8
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-11-02 12:18:13 -0500

    render movie function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// test
+document.addEventListener('DOMContentLoaded', function () {
+    function renderMovies(movieArray) {
+        finalHTML = ""
+        moviesHTML = movieArray.map(function (currentMovie) {
+            return `<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
+            <div class="card-body">
+            <p class="card-text">${currentMovie.Title}</p>
+            </div>
+            </div>`
+        })
+        finalHTML += moviesHTML.join()
+        return finalHTML
+    }
+    // let content = document.getElementById("movie");
+    // content.innerHTML = (renderMovies(movieData))
+    
+})
+
