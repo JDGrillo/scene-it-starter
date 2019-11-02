430c845833ca5faaee8a9808436c9deab418d69a
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-11-02 13:57:17 -0500

    need to work on watchlist

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
             <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
             <div class="card-body">
             <p class="card-text">${currentMovie.Title} ${currentMovie.Year}</p>
-            <button class="btn btn-primary input-group-btn" type="submit" onclick="saveToWatchlist(${currentMovie.imdbID})">Add</button>
+            <button class="btn btn-primary input-group-btn" type="submit" onclick="saveToWatchlist('${currentMovie.imdbID}')">Add</button>
             </div>
             </div>`
         })
         return moviesHTML.join()
     }
-      
+
     let content = document.getElementsByClassName("movie")[0];
     // document.getElementById("searchButton").addEventListener("submit", renderMovies(movieData))
     // content.innerHTML = (renderMovies(movieData))   
-    $('#searchButton').click(function(e) {
+    $('#searchButton').click(function (e) {
         e.preventDefault()
         content.innerHTML = renderMovies(movieData)
         console.log(renderMovies(movieData))
@@ -23,9 +23,19 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 function saveToWatchlist(imdb) {
-    let movie = movieData.find(function(currentMovie){
+    let movie = movieData.find(function (currentMovie) {
         return currentMovie.imdbID == imdb
     })
-    console.log(movie)
+    let watchlistJSON = localStorage.getItem('watchlist')
+    let watchlist = JSON.parse(watchlistJSON)
+    if (watchlist === null) {
+        watchlist = []
+    }
+    else {
+        watchlist.push(movie)
+        watchlistJSON = JSON.stringify(watchlist)
+        localStorage.setItem('watchlist', watchlistJSON)
+    }
+
 }
 
